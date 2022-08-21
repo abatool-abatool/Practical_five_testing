64c5af07a8e05ee75ccfb4405df63d9c104e1de9
abatool-abatool <amna.deakin@gmail.com>
2022-08-21 23:22:07 +1000

    week 5 updates

---
+++ routes/projectRoutes.js
@@ -0,0 +1,49 @@
+var express = require("express");
+var router = express.Router();
+
+let client = require("../dbConnect");
+let projectCollection;
+setTimeout(() => {
+    projectCollection = client.mongoClient.db().collection("projects");
+}, 2000)
+
+
+//insert project......
+const insertProjects = (project, callback) => {
+    projectCollection.insert(project,callback);
+}
+
+//get project....
+const getProjects = (callback) => {
+    projectCollection.find({}).toArray(callback);
+}
+
+router.get('/', (req, res) => {
+    getProjects((err,result) => {
+        if(err) {
+            res.json({statusCode: 400, message: err})
+        }
+        else {
+            res.json({statusCode: 200, message:"Success", data: result})
+        }
+    })
+})
+
+router.post('/', (req, res) => {
+    console.log("New Project added", req.body)
+    var newProject = req.body;
+    insertProjects(newProject,(err,result) => {
+        if(err) {
+            res.json({statusCode: 400, message: err})
+        }
+        else {
+            res.json({statusCode: 200, message:"Project Successfully added", data: result})
+        }
+    })
+
+})
+
+
+
+
+module.exports = router;
